3cd58fde9f75d40e3946d59817e76be9a8695301
askrajeshdg-sketch <askrajeshdg@gmail.com>
2026-02-22 15:44:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,26 +7,30 @@ app = Flask(__name__)
 CORS(app)
 
 # Connect to RDS
-connection = pymysql.connect(
-    host=os.environ["DB_HOST"], 
-    user=os.environ["DB_USER"],
-    password=os.environ["DB_PASSWORD"],
-    database=os.environ["DB_NAME"]
-)
+def get_connection(): 
+    return pymysql.connect(
+        host=os.environ["DB_HOST"],
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
+        database=os.environ["DB_NAME"],
+        cursorclass=pymysql.cursors.DictCursor
+    )
 
 @app.get("/student")
 def get_student():
     name = request.args.get("name")
-
+    
+    connection=get_connection()
     cursor = connection.cursor()
     cursor.execute("SELECT score FROM students WHERE name=%s", (name,))
     row = cursor.fetchone()
-
+    connection.close()
     if row:
         return jsonify({"found": True, "score": row[0]})
     else:
         return jsonify({"found": False})
 
 app.run(host="0.0.0.0", port=5000)
+
 
 
